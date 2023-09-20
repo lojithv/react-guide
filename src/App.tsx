3adca2eac9c04770f7c9b3495a1bed3ea7ac0ec9
Lojith Vinsuka <lojithvinsuka1998@gmail.com>
2023-09-20 14:33:21 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // Function to increment the count
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
