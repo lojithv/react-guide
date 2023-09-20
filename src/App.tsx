9ca0aef62c178e180e5c5db5e93884ca06228e08
Lojith Vinsuka <lojithvinsuka1998@gmail.com>
2023-09-20 15:15:22 +0530

    usecontext example 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useState } from 'react';
 
+// Step 1: Create a Context
+const AppContext = createContext({} as any);
+
+// Step 2: Create a Provider
+function AppProvider({ children }:any) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <AppContext.Provider value={{ count, setCount }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+// Step 3: Create a Custom Hook to use the Context
+function useAppContext() {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('useAppContext must be used within an AppProvider');
+  }
+  return context;
+}
+
+// Component using the Context
+function Counter() {
+  const { count, setCount } = useAppContext();
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+// Using the AppProvider to wrap the component tree
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <Counter />
+    </AppProvider>
   );
 }
 
