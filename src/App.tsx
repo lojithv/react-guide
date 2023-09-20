9bf162520d93b82a8f23326d54269dac48bfc0b6
Lojith Vinsuka <lojithvinsuka1998@gmail.com>
2023-09-20 15:11:27 +0530

    useref example 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,22 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-// Step 1: Create a Context
-const AppContext = createContext({} as any);
-
-// Step 2: Create a Provider
-function AppProvider({ children }:any) {
-  const [user, setUser] = useState(null);
-
-  return (
-    <AppContext.Provider value={{ user, setUser }}>
-      {children}
-    </AppContext.Provider>
-  );
-}
-
-// Step 3: Create a Custom Hook to use the Context
-function useAppContext() {
-  const context = useContext(AppContext);
-  if (!context) {
-    throw new Error('useAppContext must be used within an AppProvider');
-  }
-  return context;
-}
-
-// Component using the Context
-function UserComponent() {
-  const { user, setUser } = useAppContext();
-
-  const handleLogin = () => {
-    setUser({ name: 'John Doe' });
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-  };
-
-  return (
-    <div>
-      {user ? (
-        <div>
-          <p>Welcome, {user.name}!</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <button onClick={handleLogin}>Login</button>
-      )}
-    </div>
-  );
-}
-
-// Additional Component using the Context
-function UserInfo() {
-  const { user } = useAppContext();
-
-  return (
-    <div>
-      {user ? (
-        <div>
-          <p>User Information:</p>
-          <p>Name: {user.name}</p>
-        </div>
-      ) : (
-        <p>Please log in</p>
-      )}
-    </div>
-  );
-}
-
-// Using the AppProvider to wrap the component tree
 function App() {
+  const inputRef = useRef(null as unknown as HTMLInputElement);
+
+  const handleClick = () => {
+    inputRef.current.focus()
+  };
+
+  useEffect(() => {
+    // inputRef.current.focus()
+  }, [])
+  
+
   return (
-    <AppProvider>
-      <UserComponent />
-      <UserInfo />
-    </AppProvider>
+    <div>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleClick}>Focus Input</button>
+    </div>
   );
 }
 
