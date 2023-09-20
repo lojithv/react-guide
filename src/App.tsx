4f3150717031996af703706d39c5e90f5c81374a
Lojith Vinsuka <lojithvinsuka1998@gmail.com>
2023-09-20 14:59:14 +0530

    functional comp example 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import useFormInput from './hooks/useForminput';
 
 function App() {
-  const firstName = useFormInput('');
-  const lastName = useFormInput('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   const handleSubmit = (e:any) => {
     e.preventDefault();
-    alert(`Submitted: ${firstName.value} ${lastName.value}`);
+    alert(`Submitted: ${firstName} ${lastName}`);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>First Name:</label>
-        <input type="text" {...firstName} />
+        <input 
+          type="text" 
+          value={firstName} 
+          onChange={(e) => setFirstName(e.target.value)} 
+        />
       </div>
       <div>
         <label>Last Name:</label>
-        <input type="text" {...lastName} />
+        <input 
+          type="text" 
+          value={lastName} 
+          onChange={(e) => setLastName(e.target.value)} 
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
